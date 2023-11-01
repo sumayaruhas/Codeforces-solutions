84f6bca49a953169007e8ffe9abc32a21477ee1d
Sumaya Rahman <127914406+sumayaruhas@users.noreply.github.com>
2023-11-01 18:55:45 +0600

    Create 1312A.cpp

---
+++ 1312A.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+// 230825430	Nov/01/2023 18:43UTC+6	sumayaruhas	1312A - Two Regular Polygons	GNU C++17	Accepted	15 ms	0 KB
+void num()
+{
+    int a , b;
+    cin >> a >> b;
+    if(a % b == 0){
+        cout << "YES" << endl;
+    }else{
+        cout << "NO" << endl;
+    }
+
+}
+int main()
+{
+    int t ;
+    cin >> t;
+    while(t--){
+        num();
+    }
+
+}
