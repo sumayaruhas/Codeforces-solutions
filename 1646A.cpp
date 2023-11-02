355ba4a6f45e4094355c2cfe5c01daa6f46642c7
Sumaya Rahman <127914406+sumayaruhas@users.noreply.github.com>
2023-11-02 19:38:28 +0600

    Create 1646A.cpp

---
+++ 1646A.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+// 230979514	Nov/02/2023 19:29UTC+6	sumayaruhas	A - Square Counting	GNU C++17	Accepted	140 ms	0 KB
+void num()
+{
+    long long int a , b;
+    cin >> a >> b;
+    long long int c = b / (a * a);
+    cout << c << endl;
+
+}
+int main()
+{
+    int t ;
+    cin >> t;
+    while(t--){
+        num();
+    }
+
+}
