bf329b7de797642a14a353c174cdb56d32fe46f5
Sumaya Rahman <127914406+sumayaruhas@users.noreply.github.com>
2023-11-04 18:49:17 +0600

    Create 1748A.cpp

---
+++ 1748A.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+// 231333885	Nov/04/2023 18:46UTC+6	sumayaruhas	1748A - The Ultimate Square	GNU C++17	Accepted	46 ms	0 KB
+void num()
+{
+    long long int n;
+    cin >> n;
+    cout << (n + 1) / 2 << endl;
+}
+int main()
+{
+    int t ;
+    cin >> t;
+    while(t--){
+        num();
+    }
+
+}
