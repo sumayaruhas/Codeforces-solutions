d2a985a56b0ba122419e95c903554c7211772f01
Sumaya Rahman <127914406+sumayaruhas@users.noreply.github.com>
2023-11-05 13:19:33 +0600

    Create 1180A.cpp

---
+++ 1180A.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+// 231422050	Nov/05/2023 12:45UTC+6	sumayaruhas	A - Alex and a Rhombus	GNU C++17	Accepted	15 ms	0 KB
+
+/* I hope you don’t forget that giving up decisively also counts as courage .
+Life is tough, and things don’t always work out well, but we should be brave and go on with our lives
+         ~ Suga
+*/
+int main()
+{
+
+    int t ;
+    cin >> t;
+    cout << (2 * t * t) - (2 * t)  + 1 << endl;
+    return 0;
+    
+}
