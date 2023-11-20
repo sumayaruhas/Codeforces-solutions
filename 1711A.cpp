6db2b77f6841baf54fa7b4db7e60952bac1f9943
Sumaya Rahman <127914406+sumayaruhas@users.noreply.github.com>
2023-11-20 19:30:13 +0600

    Create 1711A.cpp

---
+++ 1711A.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// 233565808	Nov/20/2023 19:03UTC+6	sumayaruhas	A - Perfect Permutation	GNU C++17	Accepted	61 ms	0 KB
+
+
+/* 
+I hope you don’t forget that giving up decisively also counts as courage .
+Life is tough, and things don’t always work out well, but we should be brave and go on with our lives
+Dream, you will fully bloom after all the hardships
+         ~ Suga
+*/
+void num()
+{
+  int n , k , o , i;
+  cin >> n;
+  cout << n << " ";
+  for(i = 1 ; i < n ; i++){
+    cout << i << " ";
+  }
+  cout << endl;
+
+}
+int main()
+{
+  int t;
+  cin >> t;
+  while(t--){
+    num();
+  }
+}
+
+
