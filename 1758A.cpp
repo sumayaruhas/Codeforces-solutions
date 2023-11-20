43afeb786f322646155f9dbd37985ac04bcf195a
Sumaya Rahman <127914406+sumayaruhas@users.noreply.github.com>
2023-11-20 19:29:14 +0600

    Create 1758A.cpp

---
+++ 1758A.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// 233568966	Nov/20/2023 19:27UTC+6	sumayaruhas	A - SSeeeeiinngg DDoouubbllee	GNU C++17	Accepted	15 ms	0 KB
+
+
+/* 
+I hope you don’t forget that giving up decisively also counts as courage .
+Life is tough, and things don’t always work out well, but we should be brave and go on with our lives
+Dream, you will fully bloom after all the hardships
+         ~ Suga
+*/
+void num()
+{
+  int i;
+  string kk;
+  cin >> kk;
+  cout << kk;
+  reverse(kk.begin() , kk.end());
+  cout << kk;
+  cout << endl;
+
+}
+int main()
+{
+  int t;
+  cin >> t;
+  while(t--){
+    num();
+  }
+}
+
+
