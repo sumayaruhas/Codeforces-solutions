4d810ab336a6c8620caaf760d266572ffe89a6d2
Sumaya Rahman <127914406+sumayaruhas@users.noreply.github.com>
2023-11-23 14:16:14 +0600

    Create 1768A.cpp

---
+++ 1768A.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+// https://codeforces.com/contest/1768/problem/A
+
+// 233873693	Nov/23/2023 13:11UTC+6	sumayaruhas	A - Greatest Convex	GNU C++17	Accepted	62 ms	0 KB
+
+/* 
+I hope you don’t forget that giving up decisively also counts as courage .
+Life is tough, and things don’t always work out well, but we should be brave and go on with our lives
+Dream, you will fully bloom after all the hardships
+         ~ Suga
+*/
+void num()
+{
+  long long int n , i;
+
+  cin >> n;
+  cout << n - 1 << endl;
+}
+
+
+int main()
+{
+  int t;
+  cin >> t;
+  while(t--){
+    num();
+  }
+}
+
+
